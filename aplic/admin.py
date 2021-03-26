ceec52e3bb4ea4ecc46ba5aec6a78bd188d70eef
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-03-26 16:35:26 -0300

    Criando tabelas

--- aplic/admin.py
+++ aplic/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
 
-from .models import Curso
+from .models import Endereco, Funcionario, Motorista, Gerente, Estoquista, Revendedora, Frete, Veiculo
 
 
-@admin.register(Curso)
-class CursoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'carga_horaria')
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('cidade', 'estado')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'cargo')
+
+
+@admin.register(Motorista)
+class MotoristaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cnh')
+
+
+@admin.register(Gerente)
+class GerenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'codigo')
+
+
+@admin.register(Estoquista)
+class EstoquistaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'codigo')
+
+
+@admin.register(Revendedora)
+class RevendedoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cnpj')
+
+
+@admin.register(Frete)
+class FreteAdmin(admin.ModelAdmin):
+    list_display = ('valor', 'codigo')
+
+
+@admin.register(Veiculo)
+class VeiculoAdmin(admin.ModelAdmin):
+    list_display = ('placa', 'cor')
--- aplic/models.py
+++ aplic/models.py
@@ -1,14 +1,95 @@
 from django.db import models
 
 
-class Curso(models.Model):
-    nome = models.CharField('Nome', max_length=50)
-    descricao = models.TextField('Descrição', max_length=400)
-    carga_horaria = models.IntegerField('Carga Horária')
+class Endereco(models.Model):
+    rua = models.CharField('Rua', max_length=50)
+    cidade = models.CharField('Cidade', max_length=50)
+    bairro = models.CharField('Bairro', max_length=50)
+    estado = models.CharField('Estado', max_length=40)
+    complemento = models.CharField('Complemento', max_length=10)
+    numero = models.IntegerField('Número')
 
     class Meta:
-        verbose_name = 'Curso'
-        verbose_name_plural = 'Cursos'
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+
+    def __str__(self):
+        return f"{self.rua} / {self.cidade} / {self.estado} / {self.numero}"
+
+
+class Funcionario(Endereco):
+    OPCOES = (
+        ('Motorista', 'Motorista'),
+        ('Estoquista', 'Estoquista'),
+        ('Gerente', 'Gerente'),
+    )
+    nome = models.CharField('Nome', max_length=50)
+    cpf = models.CharField('CPF', max_length=11)
+    telefone = models.CharField('Telefone', max_length=11, help_text='DD NNNNN-NNNN')
+    cargo = models.CharField('Cargo', max_length=40, choices=OPCOES)
+    salario = models.CharField('Salário', max_length=10)
+
+    class Meta:
+        verbose_name = 'Funcionario'
+        verbose_name_plural = 'Funcionarios'
 
     def __str__(self):
         return self.nome
+
+
+class Motorista(Funcionario):
+    cnh = models.CharField('CNH', max_length=11)
+
+    class Meta:
+        verbose_name = 'Motorista'
+        verbose_name_plural = 'Motoristas'
+
+
+class Gerente(Funcionario):
+    codigo = models.CharField('Código', max_length=20)
+
+    class Meta:
+        verbose_name = 'Gerente'
+        verbose_name_plural = 'Gerentes'
+
+
+class Estoquista(Funcionario):
+    codigo = models.CharField('Código', max_length=20)
+
+    class Meta:
+        verbose_name = 'Estoquista'
+        verbose_name_plural = 'Estoquistas'
+
+
+class Revendedora(Endereco):
+    nome = models.CharField('Nome', max_length=50)
+    cnpj = models.CharField('CNPJ', max_length=15)
+
+    class Meta:
+        verbose_name = 'Revendedora'
+        verbose_name_plural = 'Revendedoras'
+
+    def __str__(self):
+        return self.nome
+
+
+class Frete(Endereco):
+    valor = models.CharField('Valor', max_length=7)
+    codigo = models.CharField('Código', max_length=20)
+
+    class Meta:
+        verbose_name = 'Frete'
+        verbose_name_plural = 'Fretes'
+
+
+class Veiculo(models.Model):
+    placa = models.CharField('Placa', max_length=7)
+    cor = models.CharField('Cor', max_length=15)
+    motorista = models.ForeignKey(Motorista, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'Veículo'
+        verbose_name_plural = 'Veículos'
+
+    def __str__(self):
+        return self.placa
